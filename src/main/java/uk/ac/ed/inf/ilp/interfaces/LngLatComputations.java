f6b376fc619f102a346a517bf040034e900114c0
MICHAEL GLIENECKE <michael@glienecke.de>
2023-04-01 20:28:30 +0100

    Initial

---
+++ src/main/java/uk/ac/ed/inf/ilp/interfaces/LngLatComputations.java
@@ -0,0 +1,59 @@
+package uk.ac.ed.inf.ilp.interfaces;
+
+import uk.ac.ed.inf.ilp.constant.SystemConstants;
+import uk.ac.ed.inf.ilp.data.LngLat;
+import uk.ac.ed.inf.ilp.data.NamedRegion;
+
+/**
+ * implement the needed computations for a LngLat
+ */
+public interface LngLatComputations {
+    /**
+     * get the distance between two positions
+     * @param startPosition is where the start is
+     * @param endPosition is where the end is
+     * @return the euclidean distance between the positions
+     */
+    double distanceTo(LngLat startPosition, LngLat endPosition);
+
+    /**
+     * check if two positions are close (<= than SystemConstants.DRONE_IS_CLOSE_DISTANCE)
+     * @param startPosition is the starting position
+     * @param otherPosition is the position to check
+     * @return if the positions are close
+     */
+    boolean isCloseTo(LngLat startPosition, LngLat otherPosition);
+
+    /**
+     * special handling shortcut for the central area. Here an implementation might add special improved processing as the central region is always rectangular
+     * @param point to be checked
+     * @param centralArea the central area
+     * @return if the point is in the central area
+     */
+    default boolean isInCentralArea(LngLat point, NamedRegion centralArea) {
+        if (centralArea == null){
+            throw new IllegalArgumentException("the named region is null");
+        }
+        if (centralArea.name().equals(SystemConstants.CENTRAL_AREA_NAME) == false) {
+            throw new IllegalArgumentException("the named region: " + centralArea.name() + " is not valid - must be: " + SystemConstants.CENTRAL_AREA_NAME);
+        }
+
+        return isInRegion(point, centralArea);
+    }
+
+    /**
+     * check if the @position is in the @region (includes the border)
+     * @param position to check
+     * @param region as a closed polygon
+     * @return if the position is inside the region (including the border)
+     */
+    boolean isInRegion(LngLat position, NamedRegion region);
+
+    /**
+     * find the next position if an @angle is applied to a @startPosition
+     * @param startPosition is where the start is
+     * @param angle is the angle to use
+     * @return the new position after the angle is used
+     */
+    LngLat nextPosition(LngLat startPosition, double angle);
+}
